39244e1b61f5e718cb4924bdbe097ff25f89d20e
A-R-P-I-T-JAIN <116783129+A-R-P-I-T-JAIN@users.noreply.github.com>
2023-12-16 14:38:45 +0530

    ll

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ const connectDatabase = require("./config/database.js");
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
